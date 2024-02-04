a71be4096a20a45c5768bfb02afeba27448100c1
Restu-Averian <restuaverianputra123@gmail.com>
2024-02-04 16:03:01 +0700

    Update graph info

--- src/components/dashboard/Charts.jsx
+++ src/components/dashboard/Charts.jsx
@@ -1,6 +1,8 @@
 import { Column } from "@ant-design/plots";
 import useFetch from "../../hooks/useFetch";
 import { useEffect, useMemo, useState } from "react";
+import { Col, Row, Typography } from "antd";
+import { formatQuota } from "../../helpers";
 
 const Charts = () => {
   const fetch = useFetch();
@@ -9,7 +11,12 @@ const Charts = () => {
 
   const getQuota = () => {
     fetch({ method: "get" }, "quota")?.then(({ data }) => {
-      setQuotaState(data);
+      setQuotaState(
+        data?.map((d) => ({
+          quotaAmount: formatQuota(d?.quotaAmount),
+          purchaseCount: d?.purchaseCount,
+        }))
+      );
     });
   };
 
@@ -18,22 +25,6 @@ const Charts = () => {
       data: quotaState,
       xField: "quotaAmount",
       yField: "purchaseCount",
-      label: {
-        // 可手动配置 label 数据标签位置
-        position: "middle",
-        // 'top', 'bottom', 'middle',
-        // 配置样式
-        style: {
-          fill: "#FFFFFF",
-          opacity: 0.6,
-        },
-      },
-      xAxis: {
-        label: {
-          autoHide: true,
-          autoRotate: false,
-        },
-      },
     };
   }, [quotaState]);
 
@@ -41,6 +32,15 @@ const Charts = () => {
     getQuota();
   }, []);
 
-  return <Column {...config} />;
+  return (
+    <Row>
+      <Col span={24}>
+        <Typography.Text style={{ fontWeight: "bold", fontSize: 18 }}>
+          Quota Graph
+        </Typography.Text>
+        <Column {...config} />
+      </Col>
+    </Row>
+  );
 };
 export default Charts;
